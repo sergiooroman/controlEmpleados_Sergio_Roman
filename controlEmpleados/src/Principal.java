e69a0ecc5312ff97e7d02ab6a2ae542beb53fbae
Sergio <s.roman.dominguez@gmail.com>
2022-01-31 08:51:20 +0100

    Líneas añadidas a método main de la clase Principal

--- controlEmpleados/src/Principal.java
+++ controlEmpleados/src/Principal.java
@@ -71,6 +71,8 @@ public class Principal extends JFrame implements ActionListener{
 
 	public static void main(String[] args) {
 		 new Principal();
+		 System.out.println("Bienvenido");
+		 System.out.println("Adios");
 		}//fin main
    
 }//fin class 
